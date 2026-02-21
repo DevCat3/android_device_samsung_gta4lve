8dbe3dc9aa40fb6e7bd86f817df6de354398ca88
DevCat3 <omarelbakery2008@gmail.com>
2026-02-21 22:12:37 +0200

    fix build error

--- device.mk
+++ device.mk
@@ -22,5 +22,5 @@ PRODUCT_PACKAGES += \
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
