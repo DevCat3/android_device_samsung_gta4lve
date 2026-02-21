2a4f1701e6cc1a993bae2f0a8cfc8ced4e924c0b
DevCat3 <omarelbakery2008@gmail.com>
2026-02-21 20:49:51 +0200

    added bootctrl to device.mk

--- device.mk
+++ device.mk
@@ -7,6 +7,19 @@
 
 LOCAL_PATH := device/samsung/gta4lve
 
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl-wrapper.recovery \
+    android.hardware.boot@1.0-impl-wrapper \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
