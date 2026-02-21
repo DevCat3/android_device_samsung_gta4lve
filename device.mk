0c45eac4247f801df74638f9edc89c28dcf77b9d
DevCat3 <omarelbakery2008@gmail.com>
2026-02-21 23:56:38 +0200

    #1 fix booting

--- device.mk
+++ device.mk
@@ -8,19 +8,22 @@
 LOCAL_PATH := device/samsung/gta4lve
 
 PRODUCT_PACKAGES += \
-    otapreopt_script \
-    update_engine \
-    update_engine_sideload \
-    update_verifier \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.0-impl-wrapper.recovery \
-    android.hardware.boot@1.0-impl-wrapper \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.$(PRODUCT_PLATFORM) \
-    bootctrl.$(PRODUCT_PLATFORM).recovery
-
-# fastbootd
-PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-service \
+    android.hardware.fastboot@1.1-service \
+    android.hardware.fastboot@1.0-impl \
     android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.1-service \
+    android.hardware.boot@1.0-impl \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery \
+    android.hardware.health@2.1-service \
+    hwservicemanager \
+    vndservicemanager
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_SHIPPING_API_LEVEL := 30
+TARGET_OTA_ASSERT_DEVICE := gta4lve
